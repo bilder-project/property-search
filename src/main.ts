fed5471806d57a92f78bb7aec3a7cd4bda4518ca
potocnikvid <vidpotocnik0@gmail.com>
2025-01-08 09:50:46 +0100

    updated swagger path

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/property-search/swaggerui', app, document);
+  SwaggerModule.setup('/property-search/openapi', app, document);
 
   await app.listen(8080);
 }
