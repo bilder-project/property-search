570eb1ed3f6293e36577f0c1134d0a11ac64a376
potocnikvid <vidpotocnik0@gmail.com>
2025-01-09 18:38:56 +0100

    added backendUrl to CORS

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,11 @@ async function bootstrap() {
 
   // Retrieve the FRONTEND_URL from the environment or use a default value
   const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
+  const backendUrl = process.env.BACKEND_URL || 'http://localhost:8080';
 
   // Enable CORS with the specified origin
   app.enableCors({
-    origin: [frontendUrl, 'http://localhost:3000'], // Allow FRONTEND_URL and localhost:3000
+    origin: [frontendUrl, backendUrl, 'http://localhost:3000'], // Allow FRONTEND_URL and localhost:3000
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
