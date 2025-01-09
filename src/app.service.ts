d5a6b83dd5b788fd8d45024f174953593e0cb871
potocnikvid <vidpotocnik0@gmail.com>
2025-01-09 21:00:38 +0100

    added kafka

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,18 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
 import { Filter } from './models/filters.model';
 import axios from 'axios';
+import { Kafka } from 'kafkajs';
 
 @Injectable()
 export class AppService {
   private supabaseUrl: string;
   private supabaseKey: string;
+  private kafka: any;
+  private producer: any;
 
   constructor(private readonly supabaseService: SupabaseService) {
     this.supabaseUrl = process.env.SUPABASE_URL;
     this.supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+    this.kafka = new Kafka({
+      clientId: 'property-search',
+      brokers: [`${process.env.KAFKA_BROKER}:${process.env.KAFKA_PORT}`],
+    });
+    this.producer = this.kafka.producer();
   }
 
   async getProperties(filters: Filter) {
+    await this.sendToKafka('property-search-events', JSON.stringify(filters));
     return this.supabaseService.fetchProperties(filters);
   }
 
@@ -40,6 +49,16 @@ export class AppService {
       console.error(error.response?.data || error.message); // Log error details
       throw error;
     }
+  }
+
+  async sendToKafka(topic: string, message: string) {
+    await this.producer.connect();
+    await this.producer.send({
+      groupId: 'logging-service-group',
+      topic: topic,
+      messages: [{ value: message }],
+    });
+    await this.producer.disconnect();
   }
 
   async healthCheck() {
